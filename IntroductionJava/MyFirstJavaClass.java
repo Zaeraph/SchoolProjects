ffcd4cabf99ee1621f0ede0c7a9a3ab0bc6ef460
Zaeraph <Out.damien768@gmail.com>
2019-09-04 11:07:02 +0200

    Update MyFirstJavaClass.java

--- IntroductionJava/MyFirstJavaClass.java
+++ IntroductionJava/MyFirstJavaClass.java
@@ -1,9 +1,8 @@
 package IntroductionJava;
-
+//Damien Out opdracht 1.1
 public class MyFirstJavaClass {
     public static void main(String[] args) {
         System.out.println("This is my first Java Class!");
     }
 }
-1.1
-    
+
